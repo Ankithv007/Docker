ec940667020e4b0afd397a32e47699ba495b6abc
Ankith b v <ankithbv007@gmail.com>
2024-09-11 22:11:14 +0530

    Update urls.py

--- mywebsite/mywebsite/urls.py
+++ mywebsite/mywebsite/urls.py
@@ -1,5 +1,7 @@
-import os
-from django.core.wsgi import get_wsgi_application
+from django.contrib import admin
+from django.urls import path, include
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mywebsite.settings')
-application = get_wsgi_application()
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('home.urls')),
+]
